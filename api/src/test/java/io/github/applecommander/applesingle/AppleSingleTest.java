369f1e5ebace17698dd72ab659a134beb2df4220
Rob Greene <robgreene@gmail.com>
2018-06-07 21:21:27 -0500

    Cleaning up imports.

--- api/src/test/java/io/github/applecommander/applesingle/AppleSingleTest.java
+++ api/src/test/java/io/github/applecommander/applesingle/AppleSingleTest.java
@@ -1,9 +1,11 @@
 package io.github.applecommander.applesingle;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
